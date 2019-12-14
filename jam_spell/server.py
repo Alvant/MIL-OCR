d03053cb0f05a4112419e90b929c7fc5814a823f
Gleb Morgachev <morgachev.gi@phystech.edu>
2019-12-15 01:40:17 +0300

    Add client-server interaction for jamspell.

--- jam_spell/server.py
+++ jam_spell/server.py
@@ -1,24 +1,49 @@
-import pika
+import traceback
+
+from jamspell import TSpellCorrector
+from pika import BlockingConnection, ConnectionParameters
 
 
-def correct(text):
-    print("correction")
-    pass
+class Corrector:
+    """
+    Class receives messages and correct their text.
+    """
+    def __init__(self):
+        self.corrector = TSpellCorrector()
+        self.corrector.LoadLangModel('en.bin')
+
+        self.connection = BlockingConnection(ConnectionParameters(
+            host="rabbit"))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue="corrector", exclusive=True)
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+
+        self.channel.basic_consume("corrector", self.callback)
+
+    def correct(self, text: str):
+        return self.corrector.FixFragment(text)
+
+    def callback(self, ch, method, properties, body):
+        print(" [x] Received by correcter {}".format(body))
+        ch.basic_publish('',
+            routing_key=properties.reply_to,
+            body=self.correct(str(body)))
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+
+    def start(self):
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+        except Exception:
+            self.channel.stop_consuming()
+            print("Error!!!\n", traceback.format_exc())
 
 
-def callback(ch, method, properties, body):
-    print(" [x] Received by correcter {}".format(body))
-
-
-def run():
-    connection = pika.BlockingConnection(pika.ConnectionParameters(
-        host="rabbit"))
-    channel = connection.channel()
-    channel.queue_declare(queue="corrector")
-    print(" [*] Waiting for messages. To exit press CTRL+C")
-    channel.basic_consume("tmp", callback, auto_ack=True)
-    channel.start_consuming()
+def main():
+    corrector = Corrector()
+    corrector.start()
 
 
 if __name__ == "__main__":
-    run()
+    main()
---
+++ text_extractor.py
@@ -0,0 +1,72 @@
+import gridfs
+
+from PIL import Image
+from pymongo import MongoClient
+from io import BytesIO
+from pika import ConnectionParameters, \
+                 BlockingConnection, \
+                 BasicProperties
+
+
+class TextExtractor:
+    REPLY_QUERY = 'amq.rabbitmq.reply-to'
+    CORRECTOR_QUERY = "corrector"
+
+    def __init__(self):
+        self.client = MongoClient("localhost", 27017)
+        self.db = self.client["image_db"]
+        self.collection = self.db["image_collection"]
+        self.fs = gridfs.GridFS(self.db)
+
+    def add_image(self, path: str) -> None:
+        with open("./img.png", "rb") as f:
+            img = f.read()
+        image_idx = self.fs.put(img)
+        meta = {
+            "imageID": image_idx,
+            "recognizedText": None,
+            "edits": None
+        }
+
+        self.collection.insert_one(meta)
+
+    def get_unhandled(self):
+        return self.collection.find({'recognizedText': None})
+
+    def get_image(self, image_id):
+        img = self.fs.get(image_id)
+        return Image.open(BytesIO(img.read()))
+
+    def reply_handler(self, ch, method_frame, properties, body):
+        print('RPC Client got reply: %s' % body)
+        print('RPC Client says bye')
+
+    def proccess_images(self):
+        connection = BlockingConnection(ConnectionParameters(
+            host='localhost'))
+        channel = connection.channel()
+
+        with connection, channel:
+            channel.basic_consume(
+                TextExtractor.REPLY_QUERY,
+                self.reply_handler,
+                auto_ack=True)
+
+            for i in range(10):
+                channel.basic_publish(exchange='',
+                    routing_key=TextExtractor.CORRECTOR_QUERY,
+                    body='Hello Womld!  ' + str(i),
+                    properties=BasicProperties(reply_to=TextExtractor.REPLY_QUERY))
+
+                print(" [x] Sent 'Hello World!'")
+
+            channel.start_consuming()
+
+
+def main():
+    exctractor = TextExtractor()
+    exctractor.proccess_images()
+
+
+if __name__ == "__main__":
+    main()
